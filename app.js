d06d09a99341ec94f4666e95845041de53712203
Luchito30 <luchodick24@gmail.com>
2022-12-07 23:17:29 -0300

    se termina el home

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { get } = require("http");
 const path = require("path");
 const port = 3030;
 const app = express();
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
 
 app.listen(port, () => console.log(`Servidor levantado en http://localhost:${port}`));
