92f4c2f0116954e4e884ae85397e2093c9713051
Luchito30 <luchodick24@gmail.com>
2023-01-15 20:22:45 -0300

    termina estilos del register

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
 app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html")));
+app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views","login.html")));
 app.get("*",(req,res) => res.sendFile(path.join(__dirname,"views","404.html")));
 
 app.listen(port, () => console.log(`Servidor levantado en http://localhost:${port}`));
